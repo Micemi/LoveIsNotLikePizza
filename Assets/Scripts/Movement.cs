a2e4d9613d756d3850cba254beeb4bfab5c3d39d
Pablyco <Pablycohacker@gmail.com>
2020-07-13 02:46:26 -0300

    Boton "yes" + Movimiento del perfil.

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -7,25 +7,31 @@ public class Movement : MonoBehaviour
    private float speed = 0f;
    private Rigidbody2D rb2d;
 
- void Start() {
+    void Start()
+    {
      
      rb2d = GetComponent<Rigidbody2D>();
 
-}
+    }
      
 
     void FixedUpdate() {
         
         
-
         rb2d.AddForce (new Vector2(speed,0));
+        
 
     }
-
-    public void holis()
+    
+    public void Rechazo()
     {
         speed = -500f;
+        
+        Debug.Log("Rechasao");
+
         Destroy(gameObject, 5);
+
+
     }
 
 
---
+++ Assets/Scripts/ProfileLerp.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProfileLerp : MonoBehaviour
+{
+    private bool shouldLerp = false;
+
+    public float timeStartedLerping;
+    public float lerpTime;
+
+    public Vector2 endPosition;
+    public Vector2 startPosition;
+
+    private void StartLerping()
+    {
+        timeStartedLerping = Time.time;
+
+        shouldLerp = true;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartLerping(); 
+
+        //Start the coroutine we define below named ExampleCoroutine.
+        StartCoroutine(ExampleCoroutine());   
+    }
+    IEnumerator ExampleCoroutine()
+    {
+        //Print the time of when the function is first called.
+        Debug.Log("Started Coroutine at timestamp : " + Time.time);
+
+        //yield on a new YieldInstruction that waits for 5 seconds.
+        yield return new WaitForSeconds(lerpTime);
+
+        //After we have waited 5 seconds print the time again.
+        Debug.Log("Finished Coroutine at timestamp : " + Time.time);
+
+        shouldLerp = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (shouldLerp)
+        {
+            transform.position = Lerp(startPosition, endPosition, timeStartedLerping, lerpTime);
+        }
+       
+
+    }
+
+    public Vector3 Lerp(Vector3 start,Vector3 end,float timeStartedLerping,float lerpTime = 1)
+    {
+        float timeSinceStarted = Time.time - timeStartedLerping;
+
+        float percentageComplete = timeSinceStarted / lerpTime;
+
+        var result = Vector3.Lerp(start, end, percentageComplete);
+
+        return result;
+    }
+}
