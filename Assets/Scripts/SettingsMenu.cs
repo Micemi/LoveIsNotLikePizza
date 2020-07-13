5804a7629eb1ca392b460eeffbbaa6022abf8849
Emi Las Heras <emperorbfd@gmail.com>
2020-07-13 01:43:06 -0300

    Escena Menu Principal! , placeholders y Scripts.

---
+++ Assets/Scripts/SettingsMenu.cs
@@ -0,0 +1,15 @@
+//  INFO
+//  Emi
+//  Implements the settings menu actions and values
+
+//  *Menu making reference https://www.youtube.com/watch?v=zc8ac_qUXQY
+
+using UnityEngine;
+
+public class SettingsMenu : MonoBehaviour {
+    public void Back(){
+        Debug.Log("Back to Menu");
+        //  the rest is in unity button properties
+    }
+
+}
