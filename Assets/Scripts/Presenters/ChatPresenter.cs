60fff57ab40dc27c14d17937f3b930cc234a8a27
Facundo Chaud <faku_imper@hotmail.com.ar>
2020-07-12 23:11:18 -0300

    Add chat timer

---
+++ Assets/Scripts/Presenters/ChatPresenter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChatPresenter : MonoBehaviour
+{
+    // TODO: TEMP WILL REMOVE
+    [SerializeField] private PizzaData pizzaData;
+    
+    private Chat chat;
+
+    public float CurrentTime => chat.CurrentTime;
+    
+    private void Awake()
+    {
+        chat = new Chat(new Pizza(pizzaData));
+    }
+
+    private void Start()
+    {
+        chat.Start();
+    }
+
+    private void Update()
+    {
+        chat.Tick(Time.deltaTime);
+    }
+}
---
+++ Assets/Scripts/Presenters/ChatTimerPresenter.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using TMPro;
+using UnityEngine;
+
+public class ChatTimerPresenter : MonoBehaviour
+{
+    [SerializeField]
+    private TextMeshProUGUI text;
+    
+    [SerializeField]
+    private ChatPresenter chat;
+
+    [Header("Danger Settings")]
+    
+    [SerializeField]
+    private float dangerThreshold = 10f;
+    
+    [SerializeField]
+    private Color dangerColor = Color.red;
+
+    private Color originalColor = Color.white;
+
+    private void Awake()
+    {
+        originalColor = text.color;
+    }
+
+    private void Update()
+    {
+        float currentTime = chat.CurrentTime;
+
+        if (currentTime > 0 && currentTime <= dangerThreshold)
+            text.color = dangerColor;
+        else
+            text.color = originalColor;
+        
+        text.text = currentTime.ToString("0.00", NumberFormatInfo.InvariantInfo);
+    }
+}
