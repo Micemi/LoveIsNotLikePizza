b1ea7f040d8aa5246b5a06a12f8159f3eb0f374c
Facundo Chaud <faku_imper@hotmail.com.ar>
2020-07-13 02:00:01 -0300

    Clean ChatEmojiContainerPresenter.cs code

--- Assets/Scripts/Presenters/Chat/ChatEmojiContainerPresenter.cs
+++ Assets/Scripts/Presenters/Chat/ChatEmojiContainerPresenter.cs
@@ -1,4 +1,4 @@
-﻿using UnityEngine;
+using UnityEngine;
 
 public class ChatEmojiContainerPresenter : MonoBehaviour
 {
@@ -8,22 +8,18 @@ public class ChatEmojiContainerPresenter : MonoBehaviour
     [SerializeField]
     private ChatPresenter chat;
 
-    private Flavor[] flavors = { Flavor.Creepy, Flavor.Cute, Flavor.Geek, Flavor.Spicy };
+    private readonly Flavor[] flavors = { Flavor.Creepy, Flavor.Cute, Flavor.Geek, Flavor.Spicy };
 
     private void OnEnable()
     {
-        emojiPresenters[0].OnEmojiClicked += ClickEmoji;
-        emojiPresenters[1].OnEmojiClicked += ClickEmoji;
-        emojiPresenters[2].OnEmojiClicked += ClickEmoji;
-        emojiPresenters[3].OnEmojiClicked += ClickEmoji;
+        for (int i = 0; i < emojiPresenters.Length; i++)
+            emojiPresenters[i].OnEmojiClicked += ClickEmoji;
     }
 
     private void OnDisable()
     {
-        emojiPresenters[0].OnEmojiClicked -= ClickEmoji;
-        emojiPresenters[1].OnEmojiClicked -= ClickEmoji;
-        emojiPresenters[2].OnEmojiClicked -= ClickEmoji;
-        emojiPresenters[3].OnEmojiClicked -= ClickEmoji;
+        for (int i = 0; i < emojiPresenters.Length; i++)
+            emojiPresenters[i].OnEmojiClicked -= ClickEmoji;
     }
 
     private void Start()
@@ -35,17 +31,13 @@ public class ChatEmojiContainerPresenter : MonoBehaviour
     {
         flavors.Shuffle();
         for (int i = 0; i < emojiPresenters.Length; i++)
-        {
             emojiPresenters[i].SetEmoji(new Emoji(EmojiData.EmojisByFlavor[flavors[i]].GetRandom()));
-        }
     }
 
     private void DisableEmojis()
     {
-        emojiPresenters[0].DisableEmoji();
-        emojiPresenters[1].DisableEmoji();
-        emojiPresenters[2].DisableEmoji();
-        emojiPresenters[3].DisableEmoji();
+        for (int i = 0; i < emojiPresenters.Length; i++)
+            emojiPresenters[i].DisableEmoji();
     }
 
     private void ClickEmoji(Emoji emoji)
