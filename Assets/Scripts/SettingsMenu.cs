b0271d815a3d09855697be3190780f05a0825d30
Emi Las Heras <emperorbfd@gmail.com>
2020-07-16 15:57:19 -0300

    Sliders cambiados por termometros + scripting and prefabs

--- Assets/Scripts/SettingsMenu.cs
+++ Assets/Scripts/SettingsMenu.cs
@@ -16,21 +16,45 @@ public class SettingsMenu : MonoBehaviour {
     private string ExposedVarNameMusic = "MusicVolume";
     private string ExposedVarNameSFX = "SFXVolume";
     
+    [SerializeField]    //  reference all sliders fire component on menu
+    private GameObject[] fireComp;
 
 
+    
     //  Master is the father of the rest of sound variables
     public void SetMasterVolume(float volume){
         //  Debug.Log(volume);
         masterAudio.SetFloat(this.ExposedVarNameMaster, volume);
+        ChangeColor(volume, 0);
+        //Debug.Log("Dentro de SetMasterVol");
     }
+
     public void SetMusicVolume(float volume){
         masterAudio.SetFloat(this.ExposedVarNameMusic, volume);
+        ChangeColor(volume, 1);
     }
     public void SetSFXVolume(float volume){
         masterAudio.SetFloat(this.ExposedVarNameSFX, volume);
+        ChangeColor(volume, 2);
     }
 
-    
+    private void ChangeColor(float vol, int objNum){
+        //Debug.Log("Dentro de ChangeColor");
+        if (vol < -33.33f)   // go cold
+        {
+            fireComp[objNum].GetComponent<ChangeFire>().GoCold();
+        }
+        else if(vol < -6.66f){  //  warm
+            fireComp[objNum].GetComponent<ChangeFire>().GoWarm();
+        }
+        else{   // hottt
+            fireComp[objNum].GetComponent<ChangeFire>().GoHOTTT();
+        }
+        //  Later we can make it more dynamic and neat, i know it isnt the best
+    }
+
+    //  Poner la imagen de fuego en el script de cada menu ya que es el controller y luego en los sliders
+
 
     //  Go Back Button
     public void Back(){
