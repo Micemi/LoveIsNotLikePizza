53b6866d7ef0782d93dda2eefc56acaa29b02233
Facundo Chaud <faku_imper@hotmail.com.ar>
2020-07-16 23:10:38 -0300

    Add SceneChangers to change scenes using Unity in game

---
+++ Assets/Scripts/Utils/SceneChanger.cs
@@ -0,0 +1,17 @@
+﻿using UnityEditor;
+using UnityEngine.SceneManagement;
+
+public static class SceneChanger
+{
+    [MenuItem("Love Pizza/Scenes/Go to start menu")]
+    public static void GoToStartMenu() => SceneManager.LoadScene("StartMenu");
+
+    [MenuItem("Love Pizza/Scenes/Go to profile")]
+    public static void GoToProfile() => SceneManager.LoadScene("ProfileScene");
+
+    [MenuItem("Love Pizza/Scenes/Go to swiper")]
+    public static void GoToSwiper() => SceneManager.LoadScene("Swiper");
+
+    [MenuItem("Love Pizza/Scenes/Go to chat")]
+    public static void GoToChat() => SceneManager.LoadScene("ChatScene");
+}
