da7af5ef6e1679e87712cb05ae0bcb1c10e71273
Emi Las Heras <emperorbfd@gmail.com>
2020-07-17 04:09:32 -0300

    A mimir

---
+++ Assets/Scripts/ChangeScene.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeScene : MonoBehaviour {
+    public void change(){
+        //  Scene manager is used from the library of unityengine
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+        Debug.Log("Changed Scene");
+    }
+}
