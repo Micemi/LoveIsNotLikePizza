4d0060e3c071a82051305ca0ed70338bff1a2890
Facundo Chaud <faku_imper@hotmail.com.ar>
2020-07-16 23:40:47 -0300

    Change scenes with key numbers

---
+++ Assets/Scripts/Utils/SceneKey.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class SceneKey : MonoBehaviour
+{
+    private void Update()
+    {
+        // if (Input.GetKeyDown("1"))
+        //     SceneChanger.GoToStartMenu();
+        if (Input.GetKeyDown("1"))
+            SceneChanger.GoToSwiper();
+        // else if (Input.GetKeyDown("2"))
+        //     SceneChanger.GoToChat();
+        else if (Input.GetKeyDown("2"))
+            SceneChanger.GoToProfile();
+    }
+}
